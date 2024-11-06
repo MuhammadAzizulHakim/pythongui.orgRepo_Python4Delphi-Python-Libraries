48f8df51cd0afe8327eb1c8bcc7a535a550a7d3e
Muhammad Azizul Hakim <19azizul92@gmail.com>
2024-11-06 20:44:38 +0700

    Add files via upload

---
+++ Article002 - Variables and Types/demo01_variablesAndTypes.py
@@ -0,0 +1,7 @@
+# First variable assignment
+number = 10
+print(number)
+
+# Second variable assignment (change the first one)
+number = 10.011
+print(number)
---
+++ Article002 - Variables and Types/demo02_variablesAndTypes.py
@@ -0,0 +1,6 @@
+# Assigning multiple values to multiple variables
+a, b, c = 10, 10.5, "Hello Python4Delphi!"
+ 
+print (a)
+print (b)
+print (c)
