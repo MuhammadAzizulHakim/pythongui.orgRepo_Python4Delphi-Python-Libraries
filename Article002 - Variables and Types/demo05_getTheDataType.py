ae2a04ffb3251936feebc1cfc19508bc115167bb
Muhammad Azizul Hakim <19azizul92@gmail.com>
2024-11-08 16:35:13 +0700

    Add files via upload

---
+++ Article002 - Variables and Types/demo05_getTheDataType.py
@@ -0,0 +1,55 @@
+# Str
+x = "Hello World"
+print(type(x))
+ 
+# Int
+y = 20
+print(type(y))
+ 
+# Float
+z = 20.5
+print(type(z))
+ 
+# Complex
+a = 1j
+print(type(a))
+ 
+# List
+b = ["apple", "banana", "cherry"]
+print(type(b))
+ 
+# Tuple
+c = ("apple", "banana", "cherry")
+print(type(c))
+ 
+# Range
+d = range(6)
+print(type(d))
+ 
+# Dict
+e = {"name":"John", "age":36}
+print(type(e))
+ 
+# Set
+f = {"apple", "banana", "cherry"}
+print(type(f))
+ 
+# Frozenset
+g = frozenset({"apple", "banana", "cherry"})
+print(type(g))
+ 
+# Bool
+h = True
+print(type(h))
+ 
+# Bytes
+i = b"Hello"
+print(type(i))
+ 
+# Bytearray
+j = bytearray(5)
+print(type(j))
+ 
+# Memoryview
+k = memoryview(bytes(5))
+print(type(k))
